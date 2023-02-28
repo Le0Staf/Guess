9712003b6f529c1ef5684def802b2613223d0a21
Leo Staf <lest0823@storsjogymnasiet.se>
2023-02-28 10:49:25 +0100

    Initial commit

---
+++ Guess/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+double n = new Random().Next(1,11);
+
+Console.WriteLine("Guess a number between 1 and 10: ");
+
+Console.WriteLine(n);
+double guess = Convert.ToDouble(Console.ReadLine());
+double guesses = 3;
+
+while (guess != n && guesses > 0)
+{
+    guesses = guesses - 1;
+
+    if (guess > n && guesses > 0)
+    {
+        Console.WriteLine("Too high");
+        Console.WriteLine("Guess again: ");
+        guess = Convert.ToDouble(Console.ReadLine());
+    }
+
+
+    else if (guess < n && guesses > 0)
+    {
+        Console.WriteLine("Too low");
+        Console.WriteLine("Guess again: ");
+        guess = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+if (guess == n && guesses > 0)
+{
+    Console.WriteLine("You guessed correct!");
+}
+
+if (guesses == 0)
+{
+    Console.WriteLine("You lose!");
+}
+
+
+
